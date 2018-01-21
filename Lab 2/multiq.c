6ff01ccf736951335ab206b5bdc0e052b3699dbd
Achal Agarwal <f2015436@pilani.bits-pilani.ac.in>
2018-01-21 15:05:55 +0530

    Implemented ADT MultiQ with primarily pointer calls and references

--- Lab 2/multiq.c
+++ Lab 2/multiq.c
@@ -1,12 +1,73 @@
-#include"multiQ.h"
+#include"multiq.h"
 MultiQ createMQ(int num){
-	MultiQ* mq = (MultiQ*)malloc(sizeof(MultiQ));
-	mq->queues = (Queue*)malloc(sizeof(num*Queue));
-	mq->num = num;
-	return mq;
+    MultiQ mq = (MultiQ)malloc(sizeof(struct multiq));
+    mq->num = num;
+    mq->queues = (Queue)malloc(num*(sizeof(struct queue)));
+    return mq;
 }
 MultiQ addMQ(MultiQ mq, Task t){
-	Task *n = (Task*)malloc(sizeof(Task));
-	*n = t;
-	
+    //using priority int value
+    int p = t->p->value;
+    Element e = (Element)malloc(sizeof(struct element));
+    e->value = t->tid->value;
+    addQ(mq->queues+(p-1)*sizeof(struct queue),e);
+    return mq;
 }
+Task nextMQ(MultiQ mq){
+    int num = mq->num;
+    Task t = (Task)malloc(sizeof(struct task));
+    for(int i = 0;i<num;i++){
+        if(isEmptyQ(mq->queues+i*sizeof(struct queue))==false){
+            t->p->value = i+1;
+            t->tid->value = front(mq->queues+i*sizeof(struct queue))->value;
+            return t;
+        }
+    }
+    //else
+    return NULL;
+}
+Task delNextMQ(MultiQ mq){
+    int num = mq->num;
+    Task t = (Task)malloc(sizeof(struct task));
+    for(int i = 0;i<num;i++){
+        if(isEmptyQ(mq->queues+i*sizeof(struct queue))==false){
+            t->p->value = i+1;
+            t->tid->value = front(mq->queues+i*sizeof(struct queue))->value;
+            delQ(mq->queues+i*sizeof(struct queue));
+            return t;
+        }
+    }
+    //else
+    return NULL;
+}
+Boolean isEmptyMQ(MultiQ mq){
+    int num = mq->num;
+    for(int i=0;i<num;i++){
+        if(isEmptyQ(mq->queues+i*sizeof(struct queue))==false)
+            return false;
+    }
+    //else
+    return true;
+}
+int sizeMQ(MultiQ mq){
+    int num = mq->num;
+    int count = 0;
+    for(int i=0;i<num;i++){
+        count+=lengthQ(mq->queues+i*sizeof(struct queue));
+    }
+    return count;
+}
+int sizeMQbyPriority(MultiQ mq, Priority p){
+    //using int value of priority
+    int pr = p->value;
+    return lengthQ(mq->queues+(pr-1)*sizeof(struct queue));
+}
+Queue getQueueFromMQ(MultiQ mq, Priority p){
+    //using int value of priority
+    return mq->queues+(p->value)*sizeof(struct queue);
+}
+
+
+
+
+
--- Lab 2/multiq.h
+++ Lab 2/multiq.h
@@ -1,14 +1,28 @@
-#include"task.h"
-typedef struct multiq{
-	int num;
-	Queue* queues;
-}MultiQ;
+#include"queue.c"
+struct taskid{
+    long value;
+};
+struct priority{
+    int value;
+};
+typedef struct taskid* TaskID;
+typedef struct priority* Priority;
+struct task{
+    TaskID tid;
+    Priority p;
+};
+typedef struct task* Task;
+struct multiq{
+    int num;
+    Queue queues;
+};
+//typedef Task Element;
+typedef struct multiq* MultiQ;
 MultiQ createMQ(int num);
-MultiQ addMQ(MultiQ mq, Tast t);
+MultiQ addMQ(MultiQ mq, Task t);
 Task nextMQ(MultiQ mq);
 Task delNextMQ(MultiQ mq);
 Boolean isEmptyMQ(MultiQ mq);
 int sizeMQ(MultiQ mq);
 int sizeMQbyPriority(MultiQ mq, Priority p);
-Queue getQueueFromMQ(MultiQ, Priority p);
-
+Queue getQueueFromMQ(MultiQ mq, Priority p);
