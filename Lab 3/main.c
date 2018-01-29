9f59ef92e8bd7fdf0ab2eb0b6845d60a1f07207c
Achal Agarwal <f2015436@pilani.bits-pilani.ac.in>
2018-01-29 22:22:09 +0530

    List generator, linear to cyclic, code validation

--- Lab 3/main.c
+++ Lab 3/main.c
@@ -1,2 +1,10 @@
-#include"list.h"
+#include"cycle.h"
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    List l = createList(10000);
+    l = createCycle(l);
+    int o = testCyclic(l);
+    printf("%d",o);
+}
 
