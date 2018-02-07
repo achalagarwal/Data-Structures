2a9df38558ad321735236c25e06a66f1d2613131
Achal Agarwal <f2015436@pilani.bits-pilani.ac.in>
2018-02-07 15:10:22 +0530

    Enhanced quicksort and implemented locality improvement algo

--- Lab 4/qsort.c
+++ Lab 4/qsort.c
@@ -8,38 +8,59 @@ void quicksort(Card array, int start, int end){
             struct card temp =array[start];
             array[start]= array[end];
             array[end] = temp;
-            return;
+        
         }
+        return;
     }
     int p = (start+end)/2;
     Card pivot = &array[p];
-    int i = start;
-    int j = end;
-    while(j-i>2){
-        while(array[i].card_number<pivot->card_number && i<j)
-            i++;
-        while(array[j].card_number>pivot->card_number&&j>i)
-            j--;
-        if(j-i>2){
-            struct card temp =array[i];
-            array[i]= array[j];
-            array[j] = temp;
-        }
-        if(j-i==2){
-        if(array[i].card_number>array[j].card_number){
-            struct card temp =array[i];
-            array[i]= array[j];
-            array[j] = temp;
-        }
+   int i = start;
+   // int j = end;
+   // while(j-i>2){
+   //     while(array[i].card_number<=pivot->card_number && i<j)
+   //         i++;
+   //     while(array[j].card_number>pivot->card_number&&j>i)
+   //         j--;
+   //     if(j-i>2){
+   //         struct card temp =array[i];
+   //         array[i]= array[j];
+   //         array[j] = temp;
+   //     }
+   //     if(j-i==2){i
+   //     if(array[i].card_number>array[j].card_number){
+   //         struct card temp = array[i];
+   //         array[i]= array[j];
+   //         array[j] = temp;
+   //     }
+   //     if(i>j){
 
+   //     }
+   //     if(i==j){
+   //         i = i-1;
+   //         j = j+1;
+   // }
+   struct card temp = array[p];
+   array[p]= array[end];
+   array[end] = temp;
+   int flag = 0;
+   int wall = start-1;
+    while(i<end){
+        if(array[i].card_number<array[end].card_number){
+            wall++;
+           struct card temp = array[i];
+           array[i]= array[wall];
+           array[wall] = temp;
+        
         }
-        if(i==j){
-            i = i-1;
-            j = j+1;
+        i++;
     }
-
-    }
-    quicksort(array,start,i);
-    quicksort(array,j,end);
+    wall++;
+   temp =  array[wall];
+   array[wall]= array[end];
+   array[end] = temp;
+   if(wall>start)
+    quicksort(array,start,wall-1);
+    if(wall<end)
+    quicksort(array,wall+1,end);
     
 }
