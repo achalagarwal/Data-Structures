7d7e6f59f931735a6380a73144d40fe1278bb278
Achal Agarwal <f2015436@pilani.bits-pilani.ac.in>
2018-02-08 18:31:12 +0530

    Added lab 5 with recursive merge sort andgrade data.

---
+++ Lab 5/list.h
@@ -0,0 +1,17 @@
+
+struct node{
+	int key; //cgpa
+	char name[10];
+	struct node* next;
+};
+typedef struct node* Node;
+
+struct list{
+	Node head;
+};
+typedef struct list* List;
+struct element{
+	float key;
+	char name[10];
+};
+typedef struct element* Element;
