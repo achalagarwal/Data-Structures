5795fae1978a1bee995fb035f820812d150edb68
Achal Agarwal <f2015436@pilani.bits-pilani.ac.in>
2018-02-27 18:53:02 +0530

    Fixed Dependencies

--- LabP/hash.h
+++ LabP/hash.h
@@ -1,3 +1,6 @@
+#ifndef BLOOMFILTER
+#include"filter.h"
+#endif
 struct hashOptions{
     int bits;
     //exclusive as 0 included
@@ -5,7 +8,4 @@ struct hashOptions{
 };
 typedef struct hashOptions* HO;
 int* hash(HO options,char* data,int size);
-
-#ifdef BLOOMFILTER
-    void hashIntoFilter(HO options, char* data,int size, BF filter);
-#endif
+void hashIntoFilter(HO options, char* data,int size, BF filter);
