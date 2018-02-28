852a54dd82da98e7e619089ccb64789d4ca509e5
Achal Agarwal <f2015436@pilani.bits-pilani.ac.in>
2018-02-28 20:26:36 +0530

    Improved Hash Function by a huge margin, added stringHash function and fixed bug in string manipulation

--- LabP/hash.c
+++ LabP/hash.c
@@ -1,10 +1,19 @@
 #include<stdlib.h>
+#include<stdio.h>
 #include"hash.h"
 #ifdef MEM
     malloc = myalloc;
     realloc = myrealloc;
     free = myfree;
 #endif
+int hashString(char* word,int size){
+    int hashAddress = 0;
+    for(int i = 0;i<size;i++)
+        hashAddress = word[i] + (hashAddress << 6) + (hashAddress << 16) +(hashAddress <<20) - hashAddress;
+//    printf("%d\n",hashAddress);
+    return hashAddress;
+}
+    //make use of perms.c for hash generation
 int *hash(HO options,char* data,int size){
     int * indices = (int*)malloc(sizeof(int)*options->bits);
     int ind;
@@ -19,20 +28,25 @@ int *hash(HO options,char* data,int size){
             dataNew[ind] =(char)( dataNew[ind]^dataNew[(ind+1)%size]);
         }
         int index1 = 0;
-        for(int i = 0;i<size;i++){
+      /*  for(int i = 0;i<size;i++){
             index1+=(i+1)*(int)dataNew[i]%options->range;
         }
+    */
+        index1 = hashString(dataNew,size);
         for(int i = 0;i<size*options->bits;i++){
              ind = i%size;
-             dataNew[ind] =(char)( (dataNew[ind]+options->bits)^dataNew[(ind - 1)%size]);
+             dataNew[ind] =(char)( (dataNew[ind]+options->bits)^dataNew[abs((ind - 1)%size)]);
         }
         int index2 = 0;
-        for(int i = 0;i<size;i++){
+        index2 = hashString(dataNew,size);
+       /* for(int i = 0;i<size;i++){
             index2 +=(size-i)*(int)dataNew[i]%options->range;
         }
-        while(index1<options->range)
+        */
+       /* while(index1<options->range)
             index1*=91;
         index1*=91;
+        */
         indices[hashes++]=abs((2*index1+3*index2)%options->range);
     }
     return indices;
