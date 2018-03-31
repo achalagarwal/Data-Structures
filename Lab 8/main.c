262b9a2c127af42ea81603a4c6f5cdb388b1c717
Achal Agarwal <f2015436@pilani.bits-pilani.ac.in>
2018-03-31 23:07:13 +0530

    Tested delete using print tree

--- Lab 8/main.c
+++ Lab 8/main.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include"BST.h"
-
+int counter = 1;
 int getR(){
 	return rand()%20+150;
 }
@@ -9,10 +9,12 @@ int getR(){
 Element createElement(){
 	Element e = (Element)malloc(sizeof(struct element));
 	e->value = getR();
+    if(e->value==160)
+        counter++;
 	return e;
 }
 Bst addElements(Bst b){
-	for(int i = 1;i<10000;i++){
+	for(int i = 1;i<100;i++){
 		b = addB(b, createElement());
 		if(i%100 == 0)
 			srand(rand());
@@ -25,8 +27,12 @@ int main(){
 	e -> value = 160;
 	Bst b = create(e);
 	b = addElements(b);
-	printf(getHeight(b));
-	for(int i = 0;i<1000;i++)
-		deleteB(b, e);
+//	printf(getHeight(b));
+	visualizeBst(b);
+    for(int i = 0;i<1000;i++){
+	b=deleteB(b, e);
+	visualizeBst(b);
+    }
+  //  visualizeBst(b);
 	printf("done");
 }
