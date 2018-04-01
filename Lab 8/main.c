c82b1bdb386a743bbaa30c9fe12c8c389354d1d9
Achal Agarwal <f2015436@pilani.bits-pilani.ac.in>
2018-04-01 11:28:29 +0530

    Added more test cases

--- Lab 8/main.c
+++ Lab 8/main.c
@@ -1,21 +1,22 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include"BST.h"
-int counter = 1;
+int counter = 140;
 int getR(){
 	return rand()%20+150;
 }
 
 Element createElement(){
 	Element e = (Element)malloc(sizeof(struct element));
-	e->value = getR();
+	e->value = counter++;
     if(e->value==160)
-        counter++;
+        counter=140;
 	return e;
 }
 Bst addElements(Bst b){
 	for(int i = 1;i<100;i++){
 		b = addB(b, createElement());
+        visualizeBst(b);
 		if(i%100 == 0)
 			srand(rand());
 	}
