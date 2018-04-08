4e0d35d3271b05acd21b841a0f7650170fe60b1b
Achal Agarwal <f2015436@pilani.bits-pilani.ac.in>
2018-04-08 23:09:04 +0530

    Added html parsing, url filtering, graph implementation, building web graph

--- Lab 10/webread.c
+++ Lab 10/webread.c
@@ -29,13 +29,80 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include <string.h>
+#include "graph.h"
 #include <curl/curl.h>
-
 static size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream)
 {
   size_t written = fwrite(ptr, size, nmemb, (FILE *)stream);
   return written;
+}
+
+int hasDoubleSlash(char* url){
+    int i  = 0;
+    while(url[i]!='\0'){
+        if(url[i]=='/')
+            if(url[i+1]=='/')
+                return 1;
+        i++;
+    }
+    return 0;
+}
+
+ char* completeUrl(char* suffix,char* prefix){
+    if(hasDoubleSlash(suffix)==1)
+        return suffix;
+    int count = 0;
+    while(prefix[count]!='\0'){
+        count++;
+    }
+   /* int count2 = 0;
+    while(suffix[count2]!='\0'){
+        count2++;
+    }
+    //count2-50
+    */
+    char* url = (char*)malloc(sizeof(char)*(51+count));
+    url[0] = '\0';
+    url = strcat(url,prefix);
+    url = strcat(url,suffix);
+    return url;
+   // return strcat(prefix,suffix);
+}
+
+int parseHTML(void* stream,char* pre){
+    int a = fgetc((FILE*)stream);
+    FILE* out = fopen("out.txt","w");
+    int index = 0;
+    int flag = 0;
+    char* url = (char*)malloc(sizeof(char)*50);
+    int pos= 0;
+    char prefix[6] = "href=\"";
+    while(a!=EOF){
+        if(flag == 1){
+            if(a==(int)'"'){
+                url[pos] = '\0';
+                pos = 0;
+                flag = 0;
+                if(hasDoubleSlash(url))
+                    fprintf(out,"%s\n",url);
+            }
+            else{
+                url[pos++] =(char)a;
+            }
+        }
+        else if(index == 5){
+            index = 0;
+            flag = 1;
+        }
+        else if((char)a == prefix[index])
+            index++;
+        else
+            index =0;
+        a = fgetc((FILE*)stream);
+    }
+    fclose(out);
+    return 1;
 }
 
 int main(void)
@@ -50,8 +117,7 @@ int main(void)
   curl_handle = curl_easy_init();
 
   /* set URL to get */
-  curl_easy_setopt(curl_handle, CURLOPT_URL, "172.24.16.12/wiki/index.html");
-
+  curl_easy_setopt(curl_handle, CURLOPT_URL, "https://en.wikipedia.org/wiki/Main_Page");
   /* no progress meter please */
   //curl_easy_setopt(curl_handle, CURLOPT_NOPROGRESS, 1L);
 
@@ -70,11 +136,28 @@ int main(void)
 
   /* get it! */
   curl_easy_perform(curl_handle);
-
+    
   /* close the web file */
   fclose(webfile);
 
+  FILE* web = fopen("mylocalfile","r");
+  printf("%d",parseHTML(web,"https://en.wikipedia.org/wiki/Main_Page\0"));
+  fclose(web);
+
+  Graph G = createGraph(10);
+  Vertex root = addVertex(G,"https://en.wikipedia.org/wiki/Main_Page\0");
+  web = fopen("out.txt","r");
+  char* text = (char*)malloc(sizeof(char)*50);
+  int a = fscanf(web,"%s",text);
+  while(a!=EOF){
+      Vertex child = addVertex(G,text);
+      if(child!=NULL)
+    G = addEdge(G,root,child);
+    a = fscanf(web,"%s",text);
+  }
+  printf("%d\n",degree(G,root)); 
   /* cleanup curl stuff */
+  fclose(web);
   curl_easy_cleanup(curl_handle);
 
   return 0;
