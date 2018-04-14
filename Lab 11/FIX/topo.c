888f7bbde70000937e6544a368c25946749f018f
Achal Agarwal <f2015436@pilani.bits-pilani.ac.in>
2018-04-14 11:22:32 +0530

    Improved topo sort

--- Lab 11/FIX/topo.c
+++ Lab 11/FIX/topo.c
@@ -1,8 +1,9 @@
 #include"topo.h"
 #include<stdlib.h>
+#include<stdio.h>
 Array topologicalSort(Graph G){
-	//int size = G->size;
-	int vertices = G->vertices;
+    int vertices = G->size;
+	//int vertices = G->vertices;
 	int* arr = (int*)malloc(sizeof(int)*G->vertices);
 	int** copy = (int**)malloc(sizeof(int*)*vertices);
 	for(int i = 0;i<vertices;i++)
@@ -10,7 +11,10 @@ Array topologicalSort(Graph G){
 	for(int i =0;i<vertices;i++)
 		for(int j = 0;j<vertices;j++)
 			copy[i][j] = G->arr[i][j];
-	int* edgeCount = (int*)malloc(sizeof(int)*vertices);
+	printf("\nFinished making a copy\n");
+    int* edgeCount = (int*)malloc(sizeof(int)*vertices);
+    for(int i = 0;i<vertices;i++)
+        edgeCount[i] = 0;
 	//init to 0
 	for(int i = 0;i<vertices;i++)
 		for(int j = 0;j<vertices;j++)
@@ -19,21 +23,22 @@ Array topologicalSort(Graph G){
 	while(index<vertices){
 	for(int i = 0;i<vertices;i++){
 		if(edgeCount[i] ==0){
-			arr[index++] =G-> v[i];
+			arr[index++] =i;
 			edgeCount[i] = -1;
 		}
 	}
 	for(int i = 0;i<vertices;i++){
 		if(edgeCount[i]>0){
-			for(int j = 0;j<vertices;j++){
-				if(G->arr[j][i] ==1){
-					if(edgeCount[j] == -1){
+			for(int j = 0;j<index;j++){
+				if(G->arr[arr[j]][i] ==1){
 						edgeCount[i] -=1;
-						G->arr[j][i] =0;}
+						G->arr[arr[j]][i] =0;}
 				}
-			}
 		}
 	}
+    
+    printf("Vertices sorted: %d",index);
+    fflush(NULL);
 	}
 	Array a = (Array)malloc(sizeof(struct array));
 	a -> vertices = arr;
