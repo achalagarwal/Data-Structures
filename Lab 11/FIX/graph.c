642044e0a3f8dab383aa5de8ccbfb4ec0a82b41e
Achal Agarwal <f2015436@pilani.bits-pilani.ac.in>
2018-04-14 11:21:46 +0530

    Added Better lookup

--- Lab 11/FIX/graph.c
+++ Lab 11/FIX/graph.c
@@ -1,5 +1,5 @@
 #include"graph.h"
-//#include<stdio.h>
+#include<stdio.h>
 #include<stdlib.h>
 #include"topo.h"
 int max =0;
@@ -28,15 +28,58 @@ List edgeList(FILE* f){
 	return l;
 }
 
+
+void sort(int *arr,int size){
+    for(int i = 1;i<size;i++){
+        int v = arr[i];
+        int j = i-1;
+        for(;j>=0;j--){
+            if(v>=arr[j]){
+                arr[j+1] = v;
+                break;
+            }
+            else{
+                arr[j+1] = arr[j];
+            }
+            
+        }
+        if(j==-1)
+            arr[0] = v;
+    }
+}
+
+int binarySearch(int*arr,int size, int v){
+    int lo = 0;
+    int hi = size-1;
+    while(lo<=hi){
+    int mid = lo+(hi-lo)/2;
+    if(arr[mid]>v)
+        hi = mid-1;
+    else if(arr[mid]<v)
+        lo = mid+1;
+    else
+        return mid;
+    }
+    return -1;
+}
+
 Graph fillGraph(Graph g, List l){
 	List n = createList();
-	while(isEmpty(l)==0){
+    int count = 0;	
+    while(isEmpty(l)==0){
+        count++;
+        if(count%288==0){
+            printf("progress %d\n",count/288);
+            fflush(NULL);
+        }
 		Edge e = getElementFront(l);
 		n = addElementToBack(n,e);
-		//lookup
-		g -> arr[e->source][e->sink] = 1;
-		g->is[e->source] =1;
-		g->is[e->sink]=1;
+		int source =binarySearch(g->v,g->size, e->source);
+        int sink = binarySearch(g->v,g->size,e->sink);
+		g -> arr[source][sink] = 1;
+        
+	//	g->is[e->source] =1;
+	//	g->is[e->sink]=1;
 		removeElementFromFront(l);
 	}
 	g -> edgeList = n;
@@ -48,6 +91,20 @@ struct temp{
 	List l;
 };
 
+void insertSorted(int* arr, int size, int value){
+int i = 0;
+    for( i = size-1;i>=0;i--){
+        if(arr[i]<=value){
+            arr[i+1] = value;
+            break;
+}
+        if(arr[i]>value)
+            arr[i+1] = arr[i];
+    }
+if(i == -1)
+    arr[0] = value;
+}
+
 struct temp getData(List l){
 	int* arr = (int*)malloc(sizeof(int)*10);
 	int size = 10;
@@ -57,29 +114,26 @@ struct temp getData(List l){
                 Edge e = getElementFront(l);
                 n = addElementToBack(n,e);
 		int i =0;
-		for(i= 0;i<count;i++){
-			if(arr[i] == e->source)
-				break;
-		}
-		if(i==count){
+		i = binarySearch(arr,count,e->source);
+		if(i==-1){
 			if(count == size){
 				size *=2;
 				arr = realloc(arr,sizeof(int)*size);
 			}
-			arr[count++] = e->source;
+            insertSorted(arr,count++,e->source);
+			//arr[count++] = e->source;
 		}
 		//add
-		for(i = 0;i<count;i++){
-			if(arr[i] == e->sink)
-				break;
-		}
-		if(i == count){
+		i = binarySearch(arr,count,e->sink);
+		if(i == -1){
 		  if(count == size){
                                 size *=2;
                                 arr = realloc(arr,sizeof(int)*size);
                         }
-                        arr[count++] = e->sink;
-                }
+            insertSorted(arr,count++,e->sink);    
+          //          arr[count++] = e->sink;
+        }
+    
 
 		//add
 
@@ -87,10 +141,14 @@ struct temp getData(List l){
                 //g->is[e->source] =1;
                 //g->is[e->sink]=1;
                 removeElementFromFront(l);
+                printf("current count: %d\n",count);
+                fflush(NULL);
         }
 	Array a = (Array)malloc(sizeof(struct array));
 	a->vertices = arr;
 	a->size = count;
+    for(int i  = 0;i<count;i++)
+        printf("%d\n",arr[i]);
 	struct temp t;
 	t.a = a;
 	t.l = n;
@@ -100,35 +158,40 @@ struct temp getData(List l){
 Graph createGraph(List l){
 	Graph g = (Graph)malloc(sizeof(struct graph));
 	struct temp t = getData(l);
-	g->size = t.a->size;
+	printf("Parsed List: %d",t.a->size);
+    fflush(NULL);
+    g->size = t.a->size;
+    int size = g->size;
+    l = t.l;
+    g->v = t.a->vertices;
 	g -> arr = (int**)malloc(sizeof(int*)*(size));
-	g->is = (int*)malloc(sizeof(int)*g->size);
+	//g->is = (int*)malloc(sizeof(int)*g->size);
 	for(int i = 0;i<size;i++)
 		g->arr[i] = (int*)malloc(sizeof(int)*size);
 	for(int i =0;i<size;i++)
 		for(int j = 0;j<size;j++)
 			g->arr[i][j] = 0;
 	g = fillGraph(g,l);
-	int total =0;
-	for(int i =0 ;i<size;i++)
-		if(g->is[i] == 1)
-			total++;
-	g->v = (int*)malloc(sizeof(int)*total);
-	int index = 0;
-	g->vertices = total;
-	for(int i = 0;i<size;i++)
-		if(g->is[i] == 1)
-			g->v[index++] = i;
-	int** copy = (int**)malloc(sizeof(int*)*g->vertices);
-	for(int i = 0;i< g->vertices;i++)
-		copy[i] = (int*)malloc(sizeof(int)*g->vertices);
-	for(int i =0;i< g->vertices;i++){
-		for(int j = 0;j< g->vertices;j++){
-			copy[i][j] = g->arr[g->v[i]][g->v[j]];
-		}
-	}
-	free(g->arr);
-	g->arr = copy;
+    printf("Finished filling adj matrix");
+//	int total =0;
+	//for(int i =0 ;i<size;i++)
+	//	if(g->is[i] == 1)			total++;
+	//g->v = (int*)malloc(sizeof(int)*total);
+//	int index = 0;
+	//g->vertices = total;
+	//for(int i = 0;i<size;i++)
+	//	if(g->is[i] == 1)
+	//		g->v[index++] = i;
+	//int** copy = (int**)malloc(sizeof(int*)*g->vertices);
+	//for(int i = 0;i< g->vertices;i++)
+	//	copy[i] = (int*)malloc(sizeof(int)*g->vertices);
+	//for(int i =0;i< g->vertices;i++){
+	//	for(int j = 0;j< g->vertices;j++){
+	//		copy[i][j] = g->arr[g->v[i]][g->v[j]];
+	//	}
+	//}
+	//free(g->arr);
+	//g->arr = copy;
 	return g;
 }
 
@@ -136,17 +199,20 @@ Graph createGraph(List l){
 
 
 int main(){
-	FILE* f = fopen("graph0.txt","r");
+	FILE* f = fopen("c.txt","r");
 	List l = edgeList(f);
 	//printf("Total Number %d\n",max);
 	/*Graph g = createGraph(l);
-	Array a = topologicalSort(g);
-	for(int i =0;i< a->size;i++)
-			printf("%d\n",a->vertices[i]);
 	*/
 	//struct temp t = getData(l);
 	//max = t.a->size-1;
+    //printf("%d\n",t.a->size);
+    //sort(t.a->vertices,t.a->size);
+    //printf("%d",binarySearch(t.a->vertices,t.a->size,1000));
 	Graph g = createGraph(l);
 	Array a = topologicalSort(g);
+	for(int i =0;i< a->size;i++)
+			printf("%d\n",a->vertices[i]);
+    
 }
 
